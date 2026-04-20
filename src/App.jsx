8146803b9d94358473d3c0ab08c79ba139f8218b
Mohammadsajibulislam <mohammadsajibulislam2024@gmail.com>
2026-04-20 23:35:13 +0600

    learned useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Footer from './components/Footer';
+import { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
-        <Navbar title="আমার নতুন চ্যানেল"/>
-      <Hero title="Code By Sajib"
-       description = "Welcome to my channel, this is my coding channel, where i share my coding journey" />
-      <Footer title="© 2026 Code By Sajib" />
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>বাড়াও</button>
     </div>
   );
 }
