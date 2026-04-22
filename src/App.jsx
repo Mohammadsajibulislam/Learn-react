17ac7fbfe7c6624b0bddf19273009c5739a47c19
Mohammadsajibulislam <mohammadsajibulislam2024@gmail.com>
2026-04-22 19:52:38 +0600

    Learned useState with 3 button- increase,dicrease,reset

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
     <div>
       <h1>Count: {count}</h1>
       <button onClick={() => setCount(count + 1)}>বাড়াও</button>
+      <button onClick={() => setCount(count - 1)}>কমাও</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
